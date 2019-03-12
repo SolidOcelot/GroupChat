56335184cf1d0edf66b392bce3e4af0a2d5f626c
“Alex” <alex_ha37@abv.bg>
2019-03-12 20:55:41 +0200

    Initial project config

---
+++ GroupChat/Controllers/AuthController.cs
@@ -0,0 +1,66 @@
+﻿using GroupChat.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PusherServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupChat.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly GroupChatContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(GroupChatContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public IActionResult ChannelAuth(string channel_name, string socket_id)
+        {
+            int group_id;
+            if (!User.Identity.IsAuthenticated)
+            {
+                return new ContentResult { Content = "Access forbidden", ContentType = "application/json" };
+            }
+
+            try
+            {
+                group_id = Int32.Parse(channel_name.Replace("private-", ""));
+            }
+            catch (FormatException e)
+            {
+                return Json(new { Content = e.Message });
+            }
+
+            var IsInChannel = _context.UserGroup
+                                      .Where(gb => gb.GroupId == group_id
+                                            && gb.UserName == _userManager.GetUserName(User))
+                                      .Count();
+
+            if (IsInChannel > 0)
+            {
+                var options = new PusherOptions
+                {
+                    Cluster = "eu",
+                    Encrypted = true
+                };
+                var pusher = new Pusher(
+                    "733746",
+                    "2197134b840b0f51ef40",
+                    "6d3361985f6d36d29def",
+                    options
+                );
+
+                var auth = pusher.Authenticate(channel_name, socket_id).ToJson();
+                return new ContentResult { Content = auth, ContentType = "application/json" };
+            }
+            return new ContentResult { Content = "Access forbidden", ContentType = "application/json" };
+        }
+    }
+}
---
+++ GroupChat/Controllers/MessageController.cs
@@ -0,0 +1,92 @@
+﻿using GroupChat.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PusherServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupChat.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessageController : Controller
+    {
+        private readonly GroupChatContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        public MessageController(GroupChatContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet("{group_id}")]
+        public IEnumerable<Message> GetById(int group_id)
+        {
+            return _context.Message.Where(gb => gb.GroupId == group_id);
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> CreateAsync([FromBody] MessageViewModel message)
+        //{
+        //    Message new_message = new Message { AddedBy = _userManager.GetUserName(User), message = message.message, GroupId = message.GroupId };
+
+        //    _context.Message.Add(new_message);
+        //    _context.SaveChanges();
+
+
+        //    //change pusher app details for custom credentials
+        //    var options = new PusherOptions
+        //    {
+        //        Cluster = "eu",
+        //        Encrypted = true
+        //    };
+        //    var pusher = new Pusher(
+        //            "733746",
+        //            "2197134b840b0f51ef40",
+        //            "6d3361985f6d36d29def",
+        //        options
+        //    );
+
+        //    //WTF SAME
+        //    var result = await pusher.TriggerAsync(
+        //        "private-" + message.GroupId,
+        //        "new_message",
+        //    new { new_message },
+        //    new TriggerOptions() { SocketId = message.SocketId });
+
+
+        //    return new ObjectResult(new { status = "success", data = new_message });
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] MessageViewModel message)
+        {
+            Message new_message = new Message { AddedBy = _userManager.GetUserName(User), message = message.message, GroupId = message.GroupId };
+
+            _context.Message.Add(new_message);
+            _context.SaveChanges();
+
+
+            var options = new PusherOptions
+            {
+                Cluster = "eu",
+                Encrypted = true
+            };
+            var pusher = new Pusher(
+                        "733746",
+                        "2197134b840b0f51ef40",
+                        "6d3361985f6d36d29def",
+                    options
+                );
+
+            var result = await pusher.TriggerAsync(
+                "private-" + message.GroupId,
+                "new_message",
+            new { new_message },
+            new TriggerOptions() { SocketId = message.SocketId });
+
+            return new ObjectResult(new { status = "success", data = new_message });
+        }
+    }
+}
